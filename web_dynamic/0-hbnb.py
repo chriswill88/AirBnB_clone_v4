a91b1a55b494de4ed0de79121d9c291cc43eed2a
chriswill88 <williechri79@gmail.com>
2020-02-16 04:42:09 -0500

    [ADD+FIX] added the templates directory and parsed the uuid query string to the url

--- web_dynamic/0-hbnb.py
+++ web_dynamic/0-hbnb.py
@@ -2,6 +2,7 @@
 """
 Flask App that integrates with AirBnB static HTML Template
 """
+import uuid
 from flask import Flask, render_template, url_for
 from models import storage
 
@@ -23,7 +24,7 @@ def teardown_db(exception):
     storage.close()
 
 
-@app.route('/hbnb')
+@app.route('/0-hbnb/')
 def hbnb_filters(the_id=None):
     """
     handles request to custom template with states, cities & amentities
@@ -34,11 +35,16 @@ def hbnb_filters(the_id=None):
     places = storage.all('Place').values()
     users = dict([user.id, "{} {}".format(user.first_name, user.last_name)]
                  for user in storage.all('User').values())
-    return render_template('100-hbnb.html',
+    # added cache_id
+    cache_id = str(uuid.uuid4())
+
+    return render_template('0-hbnb.html',
                            states=states,
                            amens=amens,
                            places=places,
-                           users=users)
+                           users=users,
+                           cache_id=cache_id)  # added cache id
+
 
 if __name__ == "__main__":
     """
