bb342fdf512766dd149dd297ac0fbbd7e25a66fb
chriswill88 <williechri79@gmail.com>
2020-02-17 21:29:56 -0500

    [ADD]

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,8 +1,19 @@
-let amens = [];
-function updateAmens () {
-  amens = [];
-  $('INPUT:checked').each((x) => amens.append(x.data-id));
-}
-window.addEventListener('DOMContentLoaded', function () {
-  $('INPUT').click(updateAmens);
-});
+window.onload = function () {
+  $('.amenities .checkbox').css('margin-left', '10px');
+  $('.amenities').css('width', 'auto');
+
+  let checkedList = function () {
+    let amens_id = [];
+    let amend_name = [];
+
+    amens_id = $('.amenities INPUT:checked').get();
+    amens_name = amens_id;
+    amens_id = amens_id.map((x) => x.dataset.id);
+    amens_name = amens_name.map((y) => y.dataset.name);
+    $('.amenities h4').text("");
+    $('.amenities h4').text(amens_name.join(", "));
+  };
+  checkedList();
+
+  $('.amenities INPUT[type="checkbox"]').on('click', checkedList);
+};
